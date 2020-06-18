15bce52606546a26937302359a0dbe922bca45bc
Sudharsan <sudharsan_sundararajan@trimble.com>
2020-06-19 02:10:33 +0530

    added github pages support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,46 +1,47 @@
 module.exports = {
+  pathPrefix: '/rs_prototype',
   siteMetadata: {
-    title: `RS Interiors`,
-    description: `Website Prototype for RS Interiors`,
-    author: `Sudharsan S`,
-    keywords: ['RS','Interior Designing','gatsby', 'sudharsan']
+    title: 'RS Interiors',
+    description: 'Website Prototype for RS Interiors',
+    author: 'Sudharsan S',
+    keywords: ['RS', 'Interior Designing', 'gatsby', 'sudharsan'],
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-sass`,
+    'gatsby-plugin-sass',
     {
-      resolve: `gatsby-plugin-google-fonts`,
+      resolve: 'gatsby-plugin-google-fonts',
       options: {
         fonts: [
-          `Montserrat`,
-          `source sans pro\:300,400,700,800` // you can also specify font weights and styles
+          'Montserrat',
+          'source sans pro\:300,400,700,800', // you can also specify font weights and styles
         ],
-        display: 'swap'
-      }
-    }
+        display: 'swap',
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-}
+};
